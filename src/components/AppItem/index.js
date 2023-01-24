01bca67e6003c2df1515cac6c506ef014955bd2b
Mayukhdevan <mayukdevansahu@yahoo.com>
2023-01-24 19:58:51 +0000

    Inital commit

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const AppItem = props => {
+  const {apps} = props
+  const {appName, imageUrl} = apps
+
+  return (
+    <li className="app-item">
+      <img className="app-image" src={imageUrl} alt={appName} />
+      <p className="app-name">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabs, changeTab, isActive} = props
+  const {displayText, tabId} = tabs
+
+  const onChangeTab = () => changeTab(tabId)
+
+  const styleTab = isActive ? 'active-tab' : ''
+
+  return (
+    <li className="tab-item">
+      <button className={styleTab} type="button" onClick={onChangeTab}>
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
